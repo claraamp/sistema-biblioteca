23646ed64f6b72fd96960b81201f37ae52d45c4a
Clara Andrade <clara.andrade264@gmail.com>
2025-07-10 13:20:15 -0300

    lógica do empréstimo pronta e organização em pastas

--- src/EmprestimoException.java
+++
@@ -1,5 +0,0 @@
-public class EmprestimoException extends Exception {
-    public EmprestimoException(String message) {
-        super(message);
-    }
-}
--- src/IRegraEmprestimo.java
+++
@@ -1,3 +0,0 @@
-public interface IRegraEmprestimo {
-    void validaEmprestimo(Usuario usuario, Livro livro) throws EmprestimoException;
-}
